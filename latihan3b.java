f8aa51735ebe34010bcd7432cb85cd3893534e17
aditsptra <aditsptra27@gmail.com>
2021-09-19 13:08:42 +0700

    latihan 3b

---
+++ latihan3b.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class latihan3b {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int bebekA, bebekB;
+        bebekA = scan.nextInt();
+        bebekB = scan.nextInt();
+        scan.close();
+        System.out.println(bebekA + bebekB);
+    }
+}
---
+++ latihan3c.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class latihan3c {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String beo = scan.nextLine();
+        scan.close();
+        System.out.println(beo);
+
+    }
+}
